4ec51fdd0c570a8dd505b27a7ab725410f83b7d2
gaoussoucamara <gaoussoucamara@gmail.com>
2016-02-19 00:12:11 +0100

    test commit

--- config/application.config.php
+++ config/application.config.php
@@ -9,7 +9,7 @@ return array(
     		'Maladie',
     ),
 
-    // These are various options for the listeners attached to the ModuleManager
+    // These are various options for the listeners attached to the ModuleManager.
     'module_listener_options' => array(
         // This should be an array of paths in which modules reside.
         // If a string key is provided, the listener will consider that a module
